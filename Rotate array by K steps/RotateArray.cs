fcef1ab724eebe43cd8abc0385cfbcc3807057eb
ahmed elhoshy <93227407+ahmed-elhoshy@users.noreply.github.com>
2025-04-12 18:57:41 +0200

    Create RotateArray.cs

---
+++ Rotate array by K steps/RotateArray.cs
@@ -0,0 +1,13 @@
+public class Solution
+{
+    public void Rotate(int[] nums, int k)
+    {
+         int n = nums.Length;
+    k = k % n;
+    Array.Reverse(nums);
+    
+    Array.Reverse(nums, 0, k);
+    Array.Reverse(nums, k, n - k);
+
+    }
+}
